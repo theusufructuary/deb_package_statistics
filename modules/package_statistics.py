c923920bea0117994801df9661fa68bc02882ec0
theusufructuary <pentamingforall@gmail.com>
2022-06-07 14:25:22 +0100

    Now retrieves and decompresses Contents files. Confirmed as working.

--- modules/package_statistics.py
+++ modules/package_statistics.py
@@ -3,7 +3,7 @@
 # Plan
 # - import libs - urllib, argparse, requests, gzip, logger
 # √ - receive command line arguments from user input using argparse
-# - use urllib for http get request to Debian mirror to create a
+# √ - use urllib for http get request to Debian mirror to create a
 #  local copy of Contents file of architecture requested by user
 # - for loop to parse each line of the Contents file and count
 # number of files associated with each package, storing results in an object
@@ -13,9 +13,18 @@
 
 import argparse
 
+import urllib.request
+
+import gzip
 
 parser = argparse.ArgumentParser(description='Select an architecture')
 parser.add_argument('arch', type=str, help='architecture')
 
 args = parser.parse_args()
-print(vars(args))
+# print('args:', vars(args))
+
+
+content = urllib.request.urlopen('http://ftp.uk.debian.org/debian/dists/stable/main/Contents-{}.gz'.format(args.arch))
+
+with gzip.open(content, 'rb') as f:
+    file_content = print(f.read(1000))
