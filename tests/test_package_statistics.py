23af87cb4c67794ce26384d126e7ff0f02b278ca
theusufructuary <pentamingforall@gmail.com>
2022-06-08 13:56:14 +0100

    basic tests written for format_lines() and further tests added in as comments

--- tests/test_package_statistics.py
+++ tests/test_package_statistics.py
@@ -11,12 +11,19 @@ class TestPackageStatistics(unittest.TestCase):
 
     def test_get_contents_file(self):
         self.assertEqual(HTTPResponse, type(package_statistics.contents))
-    # Test logging
+        # To add in: test logging to syslog works as expected.
+        # Also test that every file on Debian mirror downloads and
+        # displays to terminal correctly
 
     def test_format_lines(self):
         self.assertEqual(dict, type(package_statistics.counter))
         self.assertNotEqual(0, package_statistics.counter.values)
-    # Test for '/' in package names
+        # To add in: test for '/' in package names
+
+    # def test_sort_list(self):
+        # To add in: test that key is string, and value is int
+        # and only 10 lines are printed to the terminal
+        # and that the sorting works as expected
 
 
 if __name__ == '__main__':
